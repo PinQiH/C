65a948070986e402a90c913ff38874dc18a33ee9
PinQiH <83577156+PinQiH@users.noreply.github.com>
2022-07-06 12:35:48 +0800

    if辨別奇數偶數

---
+++ 4-14-2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	int num;
+	printf("叫块俱计");
+	scanf("%d", &num);
+	if (num % 2 == 0)
+	{
+		printf("案计");
+	}
+	else
+	{
+		printf("计");
+	}
+	system("PAUSE");
+	return 0;
+}
