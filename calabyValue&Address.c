c4290e33406e827bdd093c004612a0f2123f96e1
PinQiH <83577156+PinQiH@users.noreply.github.com>
2022-07-06 12:28:02 +0800

    *指標練習

--- calabyValue&Address.c
+++ calabyValue&Address.c
@@ -11,7 +11,7 @@ int main()
     int x=100,y=400;
     printf("call by value\n");
     printf("befor swap: x= %d, y= %d\n", x, y);
-    swap1(x,y);//傳送值 
+    swap1(x,y); //傳送值 
     printf("after swap: x= %d, y= %d\n", x, y);
     
     printf("\n");
@@ -20,13 +20,13 @@ int main()
     int a=100,b=400;
     printf("call by address\n");
     printf("befor swap: a= %d, b= %d\n", a, b);
-    swap2 (&a, &b);//傳送地址 
+    swap2 (&a, &b); //傳送地址 
     printf("after swap: a= %d, b= %d\n", a, b);
     system("PAUSE");
     return 0;
 }
 
-void swap1 (int x, int y)//接收值 
+void swap1 (int x, int y) //接收值 
 {
      int temp;
      temp=x;
@@ -35,12 +35,12 @@ void swap1 (int x, int y)//接收值
      printf("after swap: a= %d, b= %d\n", x, y);
  }
  
- void swap2 (int *x, int *y)//接收地址，用*打開取出地址內存取的值 
+ void swap2 (int *x, int *y) //接收地址，用*打開取出地址內存取的值 
 {
      int temp;
-     temp=*x;
-     *x= *y;
-     *y=temp;
+     temp = *x;
+     *x = *y;
+     *y = temp;
      printf("after swap: a= %d, b= %d\n", *x, *y);
  }
 
